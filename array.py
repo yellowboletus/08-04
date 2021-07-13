5437eecca2c869f62e24ac8c5823173033fb084a
Sonja <s.zuercher@websan.ch>
2021-07-13 20:37:38 +0200

    08-04 1. Ex. Your favorite songs

---
+++ array.py
@@ -0,0 +1,8 @@
+songs = ["I will survive", 
+        "Amerika", 
+        "Roses", 
+        "Für immer uf di"]
+print(songs[0]) #arrays are 0-based!!!
+print(songs[1])
+print(songs[2])
+print(songs[3])
